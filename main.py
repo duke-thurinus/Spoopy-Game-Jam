9bfb1fd27df6eeafa358a35616f63b9c1354946d
unknown <henry.balch1@marist.edu>
2017-10-27 20:53:40 -0400

    made map matrix

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 
+def initilize():
+    galaxy = [  ["Eta Helion", "Agromega", "Pi Abbidon"]#Sol
+               ,["Agromega", "Sol"]#Eta Helion
+               ,["Eta Helion", "Eurian", "Sol"]#Agromega
+               ,["Sol", "Devolin"]#Pi Abbidon
+               ,["Pi Abbidon", "Euridian", "Ross 128"]#Devolin
+               ,["Agromega", "Acrux", "Carina 369", "Devolin"]#Euridian
+               ,["Euridian"]#Acrux
+               ,["Euridian", "Ross 128"]#Carina 369
+               ,["Devolin", "Carina 369"]#Ross 128
+        ]
+
 def print_text():
     pass
 
@@ -12,6 +24,7 @@ def use_command(command):
     pass
 
 def loop():
+    initilize()
     while True:
         print(print_text())
         use_command(get_command())
